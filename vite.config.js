284d94f106b2285493976b8ac39a0d25a61f1812
Ease <your-email@example.com>
2026-07-06 13:26:29 +0800

    fix: update base path to /inav-tool/ for new repo name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/inavtool/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/inav-tool/' : '/',
   server: {
     port: 3000,
     open: true
